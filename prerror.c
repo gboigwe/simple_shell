a24270753e2fd2f18a739084f918c96a96754a9c
BlessedF <faitholotu196@gmail.com>
2023-12-12 05:49:19 -0800

    Print error

---
+++ prerror.c
@@ -0,0 +1,23 @@
+#include "shell.h"
+/**
+ * pr_error - Print custom error message
+ * @argv: Program name
+ * @c: Error count
+ * @cmd: Command
+ *
+ * Return: void
+ */
+void pr_error(char **argv, int c, char **cmd)
+{
+ char *er = _itochar(c);
+ PRINTER(argv[0]);
+ PRINTER(": ");
+ PRINTER(er);
+ PRINTER(": ");
+ PRINTER(cmd[0]);
+ PRINTER(": Illegal number: ");
+ PRINTER(cmd[1]);
+ PRINTER("\n");
+ free(er);
+}
+
